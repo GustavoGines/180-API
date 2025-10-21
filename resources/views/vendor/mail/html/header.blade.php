6671c85da85d1b5428a5a5a8ba0bcb9095190516
Gustavo Gines <ginesparker95@gmail.com>
2025-10-21 02:59:28 -0300

    Personalizar plantilla de email con logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ config('app.url') }}" style="display: inline-block;">
             <img src="{{ config('app.url') }}/images/logo_180.png" alt="180° Pastelería Logo"
-                style="height: 150px; width: auto; border: 0;">
+                style="height: 350px; width: auto; border: 0;">
         </a>
     </td>
 </tr>
