01fd07fb965aa7048a643cafea911518c6a6f3c2
Gustavo Gines <ginesparker95@gmail.com>
2025-10-22 00:15:05 -0300

    Modificando la Ruta para mostrar pedidos por id

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
     Route::put('/orders/{order}', [OrderController::class, 'update']);
     Route::delete('/orders/{order}', [OrderController::class, 'destroy']);
-    Route::get('/orders/{order}', [OrderController::class, 'index']);
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
 
     // Devices (FCM)
     Route::post('/devices/register', [DeviceController::class, 'store']);
