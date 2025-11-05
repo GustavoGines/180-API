bb7ebf2c450e131b781167955abd9788dcff68d0
Gustavo Gines <ginesparker95@gmail.com>
2025-11-05 01:05:53 -0300

    fix: probando notificaciones

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:send-tomorrow-notifications')->everyFiveMinutes();
+Schedule::command('app:send-tomorrow-notifications')
+    ->everyFiveMinutes()
+    ->appendOutputTo(storage_path('logs/laravel.log'));
