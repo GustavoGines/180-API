ef12b0c49b52b231a29609518f54719cfc326d41
Gustavo Gines <ginesparker95@gmail.com>
2025-11-07 01:45:33 -0300

    Se agrego que al cerrar sesion se borre el token del dispositivo de la base de datos

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -47,5 +48,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Define la relación "uno a muchos" con los dispositivos del usuario.
+     */
+    public function devices(): HasMany
+    {
+        // Asume que tu modelo se llama 'Device'
+        // y que la tabla 'devices' tiene una columna 'user_id'
+        return $this->hasMany(Device::class); 
     }
 }
