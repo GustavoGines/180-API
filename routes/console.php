e53efe045e3703985d66af053e6ff792d30e32a8
GustavoGines <ginesparker95@gmail.com>
2025-12-26 02:42:25 -0300

    fix: use command signatures in console schedule to avoid early firebase instantiation

--- routes/console.php
+++ routes/console.php
@@ -2,21 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Console\Commands\SendTomorrowOrderNotifications;
-use App\Console\Commands\SendTodayOrderNotifications;
-use Illuminate\Support\Facades\Schedule; 
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(SendTomorrowOrderNotifications::class) 
+// Usamos los strings de las firmas directamente para evitar instanciar los comandos (y sus dependencias como Firebase) al iniciar la app
+Schedule::command('app:send-tomorrow-notifications')
     ->everyFiveMinutes()
     ->appendOutputTo(storage_path('logs/laravel.log'));
 
-Schedule::command(SendTodayOrderNotifications::class)
+Schedule::command('app:send-today-notifications')
     ->dailyAt('08:00') // <-- Se ejecuta todos los días a las 8:00 AM
     ->appendOutputTo(storage_path('logs/laravel.log'));
-
 
 Schedule::command('sanctum:prune-expired')->daily();
